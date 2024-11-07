20da0f1f7c308db02d3895fe2654379498535aaf
Amxx79 <e.siwanov@gmail.com>
2024-11-08 01:31:49 +0200

    Data Seeding: Products

--- TableTree.Data/Configuration/ProductConfiguration.cs
+++ TableTree.Data/Configuration/ProductConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Diagnostics;
 using TableTree.Data.Models;
 using static TableTree.Common.ApplicationConstants;
 
@@ -33,52 +34,50 @@ namespace TableTree.Data.Configuration
                 .Property(p => p.ImageUrl)
                 .HasDefaultValue(NoImageUrl);
 
-            //builder
-            //    .HasData(this.GenerateProducts());
+            builder
+                .HasData(this.GenerateProducts());
         }
 
-        /*private IEnumerable<Product> GenerateProducts()
+        private IEnumerable<Product> GenerateProducts()
         {
             IEnumerable<Product> products = new List<Product>()
             {
                 new Product()
                 {
                     Id = Guid.NewGuid(),
-                    Name = "Round Epoxy Table Moss Stones",
-                    Description = "Round Epoxy Tables have gradually shifted from designer fads to sought out and indispensable home decor for those who want to keep it stylish",
-                    Price = 526.50m,
+                    Name = "Rustic Live Edge Tree Table",
+                    Description = "A handcrafted table made from live-edge oak, showcasing natural wood grain and organic shape. Perfect as a centerpiece in any living space.",
+                    Price = 799.99m,
                     ImageUrl = "https://i.etsystatic.com/21622583/r/il/745996/5104378769/il_794xN.5104378769_dck9.jpg",
                     IsDeleted = false,
-                    TreeTypeId = Guid.Parse("401DD5CD-C271-4960-84CE-0B364C96F039"),
-                    CategoryId = Guid.Parse("61BC3294-73CA-441B-9B53-0D4F26B673F3"),
+                    CategoryId = Guid.Parse("A1042FEE-F95A-4BF3-A758-49B13CFF3E79"),
+                    TreeTypeId = Guid.Parse("632BFA2F-AA06-4C1B-913D-33EF43A44D34"),
                 },
-
-                new Product()
+                new Product
                 {
                     Id = Guid.NewGuid(),
-                    Name = "Rustic Tree Trunk Coffee Table",
-                    Description = "Resin Cast Rustic Tree Trunk Side Table, Such a unique and beautiful Side Table fit for any room around your home. We custom make these Rustic Tree Trunk Side Tables to any style,size and colour of Epoxy Resin. We also manufacture our own legs and can accommodate a number of styles to suit your needs",
-                    Price = 999.90m,
+                    Name = "Epoxy Resin River Table",
+                    Description = "Beautiful table featuring a blue resin river down the center, crafted from black walnut wood. Ideal for dining rooms.",
+                    Price = 1199.50m,
                     ImageUrl = "https://theindustrialfurniture.co.uk/cdn/shop/files/EkranResmi2023-04-3011.47-PhotoRoom_grande.png?v=1692620377",
                     IsDeleted = false,
-                    TreeTypeId = Guid.Parse("133A6AF3-1B7D-4D6E-AA41-33BE63184766"),
                     CategoryId = Guid.Parse("A1042FEE-F95A-4BF3-A758-49B13CFF3E79"),
+                    TreeTypeId = Guid.Parse("133A6AF3-1B7D-4D6E-AA41-33BE63184766"),
                 },
-
-                new Product()
+                new Product
                 {
                     Id = Guid.NewGuid(),
-                    Name = "Waterproof natural wood bathroom cabinet with natural towel opening.",
-                    Description = "Custom double tree stump vanity design features western cedar tree stumps. It also features a redwood live-edge wood countertop. Water bridge bathroom faucets spill into hammered copper vessel sinks. Custom forged chains add to the rustic ambiance. Our double tree stump vanities are built to order, thus allowing for a complete bespoke design experience.",
-                    Price = 599.99m,
+                    Name = "Reclaimed Wood Counter Top",
+                    Description = "A sturdy coffee table made from reclaimed pine with a rich, warm finish. Eco-friendly and unique, it brings warmth to any room.",
+                    Price = 450.00m,
                     ImageUrl = "https://masivno.com/wp-content/uploads/2023/07/IMG_2392-1612x1655.jpeg",
                     IsDeleted = false,
-                    TreeTypeId = Guid.Parse("632BFA2F-AA06-4C1B-913D-33EF43A44D34"),
-                    CategoryId = Guid.Parse("6ACD5822-A59E-4427-B8AC-FBCA0751CD98"),
-                },
+                    CategoryId = Guid.Parse("61BC3294-73CA-441B-9B53-0D4F26B673F3"),
+                    TreeTypeId = Guid.Parse("401DD5CD-C271-4960-84CE-0B364C96F039"),
+                }
             };
 
             return products;
-        }*/
+        }
     }
 }
---
+++ TableTree.Data/Migrations/20241107232720_Product_Data_Seeding.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TableTree.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Product_Data_Seeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageUrl", "IsDeleted", "Name", "Price", "TreeTypeId" },
+                values: new object[,]
+                {
+                    { new Guid("7fee0ff1-3a34-4c14-9ed1-d72baa056554"), Guid.Parse("a1042fee-f95a-4bf3-a758-49b13cff3e79"), "Beautiful table featuring a blue resin river down the center, crafted from black walnut wood. Ideal for dining rooms.", "https://theindustrialfurniture.co.uk/cdn/shop/files/EkranResmi2023-04-3011.47-PhotoRoom_grande.png?v=1692620377", false, "Epoxy Resin River Table", 1199.50m, Guid.Parse("133a6af3-1b7d-4d6e-aa41-33be63184766") },
+                    { new Guid("8ce257d0-b291-49e2-a827-41b2ac20293b"), Guid.Parse("61bc3294-73ca-441b-9b53-0d4f26b673f3"), "A sturdy coffee table made from reclaimed pine with a rich, warm finish. Eco-friendly and unique, it brings warmth to any room.", "https://masivno.com/wp-content/uploads/2023/07/IMG_2392-1612x1655.jpeg", false, "Reclaimed Wood Counter Top", 450.00m, Guid.Parse("401dd5cd-c271-4960-84ce-0b364c96f039") },
+                    { new Guid("c43d04e7-1304-4662-9614-eda1c7e0d145"), Guid.Parse("a1042fee-f95a-4bf3-a758-49b13cff3e79"), "A handcrafted table made from live-edge oak, showcasing natural wood grain and organic shape. Perfect as a centerpiece in any living space.", "https://i.etsystatic.com/21622583/r/il/745996/5104378769/il_794xN.5104378769_dck9.jpg", false, "Rustic Live Edge Tree Table", 799.99m, Guid.Parse("632bfa2f-aa06-4c1b-913d-33ef43a44d34") }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("7fee0ff1-3a34-4c14-9ed1-d72baa056554"));
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("8ce257d0-b291-49e2-a827-41b2ac20293b"));
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c43d04e7-1304-4662-9614-eda1c7e0d145"));
+        }
+    }
+}
