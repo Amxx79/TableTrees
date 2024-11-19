97ff2ad27b149455fd6efa7a53f9deaefb03dcce
Amxx79 <e.siwanov@gmail.com>
2024-11-20 00:31:54 +0200

    View for Account Management added

---
+++ TableTree.Web.ViewModels/Account/UserRoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TableTree.Web.ViewModels.Account
+{
+    public class UserRoleViewModel
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
--- TableTree.Web/Controllers/AccountController.cs
+++ TableTree.Web/Controllers/AccountController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Principal;
 using TableTree.Data.Models;
 using TableTree.Services.Data.Interfaces;
+using TableTree.Web.ViewModels.Account;
 
 namespace TableTree.Web.Controllers
 {
@@ -13,12 +15,34 @@ namespace TableTree.Web.Controllers
         private readonly UserManager<ApplicationUser> userManager;
         private readonly IAccountService accountService;
 
-        public AccountController(RoleManager<ApplicationRole> roleManager, UserManager<ApplicationUser> userManager
-            , IAccountService accountService)
+        public AccountController(RoleManager<ApplicationRole> roleManager, UserManager<ApplicationUser> userManager, 
+            IAccountService accountService)
         {
             this.roleManager = roleManager;
             this.userManager = userManager;
             this.accountService = accountService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<UserRoleViewModel> model = new List<UserRoleViewModel>();
+
+
+            foreach (var user in userManager.Users)
+            {
+                var roles = await userManager.GetRolesAsync(user);
+
+                UserRoleViewModel userRole = new UserRoleViewModel()
+                {
+                    Id = user.Id.ToString(),
+                    Username = user.UserName,
+                    IsAdmin = roles.Contains("Admin") ? true : false,
+                };
+
+                model.Add(userRole);
+            }
+
+            return this.View(model);
         }
 
 
