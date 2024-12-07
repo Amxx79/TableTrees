ccc407bb1ed19a96630546a53f03897556135407
Amxx79 <e.siwanov@gmail.com>
2024-12-07 02:26:59 +0200

    Testing: Favourite Service - Get All

---
+++ TableTree.Tests/FavouriteService.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using TableTree.Data.Models;
+using TableTree.Web.Controllers;
+using TableTree.Web.ViewModels.Favourite;
+
+namespace TableTree.Services.Data.Testting
+{
+	[TestFixture]
+	public class FavouriteService
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public async Task FavouriteServiceReturns_GetAllInFavourites()
+		{
+			var mockedService = new Mock<Data.Interfaces.IFavouriteService>();
+
+			mockedService.Setup(s => s.GetAllProductsInFavouritesAsync())
+				.ReturnsAsync(GetFavouriteProductsToReturnsAsProductViewModel().Result.AsEnumerable());
+
+			var controller = new FavouritesController(mockedService.Object);
+
+			var result = await controller.Index();
+			var viewResult = result as ViewResult;
+			var model = viewResult.Model as IEnumerable<ProductViewModel>;
+
+			Assert.That(result, Is.Not.Null);
+			Assert.That(model.First().Name == "Epoxy Resing");
+			Assert.That(model.Last().TreeType == "Beech");
+		}	
+
+		private List<FavouriteProduct>GetFavouriteProductsToReturns()
+		{
+			var favouriteProducts = new List<FavouriteProduct>() 
+			{
+				new FavouriteProduct()
+				{
+					ApplicationUserId = Guid.NewGuid(),
+					ProductId = Guid.NewGuid(),
+				},
+				new FavouriteProduct()
+				{
+					ApplicationUserId = Guid.NewGuid(),
+					ProductId = Guid.NewGuid(),
+				},
+				new FavouriteProduct()
+				{
+					ApplicationUserId = Guid.NewGuid(),
+					ProductId = Guid.NewGuid(),
+				},
+			};
+
+			return favouriteProducts;
+		}
+
+		private async Task<List<ProductViewModel>> GetFavouriteProductsToReturnsAsProductViewModel()
+		{
+			var products = new List<ProductViewModel>()
+			{
+				new ProductViewModel()
+				{
+					Id = Guid.NewGuid(),
+					Name = "Epoxy Resing",
+					Price = 999m,
+					ImageUrl = "",
+					TreeType = "Oak",
+				},
+				new ProductViewModel()
+				{
+					Id = Guid.NewGuid(),
+					Name = "Test test",
+					Price = 111m,
+					ImageUrl = "",
+					TreeType = "Beech",
+				},
+			};
+
+			return await Task.FromResult(products);
+		}
+
+	}
+}
