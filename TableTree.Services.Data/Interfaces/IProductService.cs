874d470bbd2fd43b9708074749bd0b5ce43e43bd
Amxx79 <e.siwanov@gmail.com>
2024-12-11 23:55:45 +0200

    Fix: Clean services

--- TableTree.Services.Data/Interfaces/IProductService.cs
+++ TableTree.Services.Data/Interfaces/IProductService.cs
@@ -5,19 +5,19 @@ namespace TableTree.Services.Data.Interfaces
 {
     public interface IProductService
     {
-        Task<AllProductsSearchFilterViewModel> GetAllProductsAsync(AllProductsSearchFilterViewModel? filters); //DONE TESTING
+        Task<AllProductsSearchFilterViewModel> GetAllProductsAsync(AllProductsSearchFilterViewModel? filters);
         Task AddProductAsync(AddProductInputModel model); //DONE TESTING
-        Task<ProductDetailsViewModel> GetProductDetailsByIdAsync(Guid id); //DONE TESTING
-        Task<EditProductViewModel> GetProductForEditByIdAsync(Guid id); //DONE TESTING
-        EditProductViewModel GetProductForEditById(Guid id); //DONE TESTING
+        Task<ProductDetailsViewModel> GetProductDetailsByIdAsync(Guid id);
+        Task<EditProductViewModel> GetProductForEditByIdAsync(Guid id);
+        EditProductViewModel GetProductForEditById(Guid id);
         Task<bool> EditProductAsync(EditProductViewModel model);
-        bool EditProduct(EditProductViewModel model); //DONE TESTING
-        Task<Product> GetProductByIdAsync(Guid id); //DONE TESTING
-		Product GetProductById(Guid id); //DONE TESTING
+        bool EditProduct(EditProductViewModel model);
+        Task<Product> GetProductByIdAsync(Guid id);
+		Product GetProductById(Guid id);
         Task AddCommentToProduct(CommentInputModel model);
 		Task<IEnumerable<Category>> GetAllCategoriesAsync();
         Task<IEnumerable<TreeType>> GetAllTreeTypesAsync();
-        IEnumerable<Category> GetAllCategories(); //DONE TESTING
+        IEnumerable<Category> GetAllCategories();
         IEnumerable<TreeType> GetAllTreeTypes();
         Task<DeleteProductViewModel> GetProductForDelete(Guid id);
         Task SoftDelete(Guid id);
